1af03ddd500f2d174ad14dd1e70493418a96c5cd
viktor.ivanov <vantek1266@gmail.com>
2025-05-26 14:09:49 +0700

    pushing to trigger diamond code review

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from aw_server.server import AWFlask
 # Выбираем метод хранения. Здесь используется, например, Peewee.
 storage_method = aw_datastore.get_storage_methods()["peewee"]
 
-# Создаём экземпляр приложения.
+# Создаём экземпляр приложения
 # Gunicorn сам управляет хостом и портом, поэтому host можно указать как "0.0.0.0".
 app = AWFlask(
     host="0.0.0.0",
